04f4f5f49ab8858770f17753cfa49ae7b99d83eb
tigrisanimus <tigrisanimus@gmail.com>
2025-12-26 06:14:25 -0500

    Harden swapback config and buyback protections

--- test/mocks/MockPair.sol
+++ test/mocks/MockPair.sol
@@ -5,10 +5,23 @@ contract MockPair {
     address public token0;
     address public token1;
     address public factory;
+    uint112 private reserve0;
+    uint112 private reserve1;
+    uint32 private blockTimestampLast;
 
     constructor(address token0_, address token1_, address factory_) {
         token0 = token0_;
         token1 = token1_;
         factory = factory_;
+    }
+
+    function setReserves(uint112 reserve0_, uint112 reserve1_) external {
+        reserve0 = reserve0_;
+        reserve1 = reserve1_;
+        blockTimestampLast = uint32(block.timestamp);
+    }
+
+    function getReserves() external view returns (uint112, uint112, uint32) {
+        return (reserve0, reserve1, blockTimestampLast);
     }
 }
